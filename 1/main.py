77c8247b419d5dc937dd0035363b64e7b06e51aa
imnomee <59981447+imnomee@users.noreply.github.com>
2020-12-15 13:34:01 +0500

    Day One

---
+++ 1/main.py
@@ -0,0 +1,4 @@
+name = input('What is your name?\n')
+nameLength = len(name)
+
+print(nameLength)
---
+++ 1/project-1.py
@@ -0,0 +1,6 @@
+print("Welcome to the band name generator.")
+city_name = input("What's the name of the city you grew up in?\n")
+print(city_name)
+pet_name = input("What's the name ofyour pet?\n")
+print(pet_name)
+print("Your band name could be "+city_name+" "+pet_name)
