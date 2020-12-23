074075437307f987ad0cc4d2144ca93f8044b75b
imnomee <59981447+imnomee@users.noreply.github.com>
2020-12-23 14:06:19 +0500

    Day 17 - Quiz Game

---
+++ 17/data.py
@@ -0,0 +1,27 @@
+question_data = [
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Gumbo is a stew that originated in Louisiana.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]},
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Furby was released in 1998.",
+        "correct_answer": "True", "incorrect_answers": ["False"]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "An eggplant is a vegetable.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Science & Nature",
+        "type": "boolean", "difficulty": "medium", "question": "Scientists accidentally killed the once known world's oldest living creature, a mollusc, known to be aged as 507 years old.", "correct_answer": "True", "incorrect_answers": ["False"]}, {"category": "Politics", "type": "boolean", "difficulty": "easy", "question": "Russia passed a law in 2013 which outlaws telling children that homosexuals exist.", "correct_answer": "True", "incorrect_answers": ["False"]}, {
+        "category": "Celebrities", "type": "boolean", "difficulty": "hard", "question": "Lady Gaga&#039;s real name is Stefani Joanne Angelina Germanotta.", "correct_answer": "True", "incorrect_answers": ["False"]}, {"category": "Entertainment: Cartoon & Animations", "type": "boolean", "difficulty": "medium", "question": "Blue Danube was one of the musical pieces featured in Disney&#039;s 1940&#039;s film Fantasia.", "correct_answer": "False", "incorrect_answers": ["True"]}, {"category": "General Knowledge", "type": "boolean", "difficulty": "medium", "question": "The commercial UK channel ITV stands for &quot;International Television&quot;.", "correct_answer": "False", "incorrect_answers": ["True"]}, {"category": "Sports", "type": "boolean", "difficulty": "medium", "question": "ATP tennis hosted several tournaments on carpet court before being replaced to reduce injuries.", "correct_answer": "True", "incorrect_answers": ["False"]}, {"category": "Entertainment: Books", "type": "boolean", "difficulty": "easy", "question": "The Harry Potter series of books, combined, are over 1,000,000 words in length.", "correct_answer": "True", "incorrect_answers": ["False"]}]
